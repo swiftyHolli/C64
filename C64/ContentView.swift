87f26ca478fd690237917a3fb35cbff2ea339fc3
swiftyHolli <h.becker@chhb.de>
2025-02-05 09:25:34 +0100

    Initial Commit

---
+++ C64/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  C64
+//
+//  Created by Holger Becker on 05.02.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
