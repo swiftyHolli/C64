9f082f232a5a6c4d5ce1d6352fb18784f6f2afb8
swiftyHolli <h.becker@chhb.de>
2025-02-08 21:53:49 +0100

    more keyboard

--- C64/C64View.swift
+++ C64/C64View.swift
@@ -24,8 +24,31 @@ struct KeyboardView: View {
     
     var body: some View {
         HStack {
+            Button("Q") {keyboard.keyPressed(62)}
+            Button("W") {keyboard.keyPressed(09)}
+            Button("E") {keyboard.keyPressed(14)}
+            Button("R") {keyboard.keyPressed(17)}
+            Button("T") {keyboard.keyPressed(22)}
+            Button("Y") {keyboard.keyPressed(25)}
+            Button("U") {keyboard.keyPressed(30)}
+            Button("I") {keyboard.keyPressed(33)}
+            Button("O") {keyboard.keyPressed(38)}
+            Button("P") {keyboard.keyPressed(41)}
+        }
+        HStack {
             Button("A") {keyboard.keyPressed(10)}
-            Button("B") {keyboard.keyPressed(28)}
+            Button("S") {keyboard.keyPressed(13)}
+            Button("D") {keyboard.keyPressed(18)}
+            Button("F") {keyboard.keyPressed(21)}
+            Button("G") {keyboard.keyPressed(26)}
+            Button("H") {keyboard.keyPressed(29)}
+            Button("J") {keyboard.keyPressed(34)}
+            Button("K") {keyboard.keyPressed(37)}
+            Button("L") {keyboard.keyPressed(42)}
+            Button(":") {keyboard.keyPressed(45)}
+            Button(";") {keyboard.keyPressed(50)}
+            Button("Enter") {keyboard.keyPressed(01)}
+
         }
     }
 }
